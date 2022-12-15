d9dcbcb7de13e3e76acf322519212ef04f21cb07
Semih Altın <80004993+SemihAltin1@users.noreply.github.com>
2022-12-15 22:28:19 +0300

    title added

--- Digit divisible by three/Main.java
+++ Digit divisible by three/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String args[])
 	{
+		System.out.print("Title: ");
 		Scanner scn=new Scanner(System.in);
 		int number,mod,i=0;
 		System.out.print("Enter Number : ");
